8d182077474cfd67062933be081c0de5bb51fc6f
Johan Nguyen <johannsevenn@gmail.com>
2025-12-04 15:52:28 -0500

    add PCAPipeline for train/val/test split with PCA transform

--- src/data/pca_transform.py
+++ src/data/pca_transform.py
@@ -42,3 +42,46 @@ class TemporalPCA:
         if not self.fitted:
             raise RuntimeError("PCA not fitted.")
         return self.pca.inverse_transform(X_pca)
+    
+def temporal_split(data, train_pct=0.7, val_pct=0.15):
+    n = len(data)
+    tr_end = int(n * train_pct)
+    val_end = int(n * (train_pct + val_pct))
+    
+    train = data[:tr_end]
+    val = data[tr_end:val_end]
+    test = data[val_end:]
+    
+    return train, val, test
+
+class PCAPipeline:
+    def __init__(self, n_comp=10, train_pct=0.7, val_pct=0.15):
+        self.pca = TemporalPCA(n_comp=n_comp)
+        self.train_pct = train_pct
+        self.val_pct = val_pct
+    
+    def process(self, X, y=None):
+        X_tr, X_val, X_te = temporal_split(X, self.train_pct, self.val_pct)
+        
+        self.pca.fit(X_tr)
+        
+        X_tr_pca = self.pca.transform(X_tr)
+        X_val_pca = self.pca.transform(X_val)
+        X_te_pca = self.pca.transform(X_te)
+        
+        result = {
+            'X_train': X_tr_pca,
+            'X_val': X_val_pca,
+            'X_test': X_te_pca,
+            'var_exp': self.pca.var_exp,
+            'total_var': self.pca.total_var,
+            'n_comp': self.pca.pca.n_components_
+        }
+        
+        if y is not None:
+            y_tr, y_val, y_te = temporal_split(y, self.train_pct, self.val_pct)
+            result['y_train'] = y_tr
+            result['y_val'] = y_val
+            result['y_test'] = y_te
+        
+        return result
